a6e0952d0e209615d255c47db8d078e2bdc8a52d
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-09-17 15:19:55 +0200

    Use kerberos authentication and text messages

--- jira.py
+++ jira.py
@@ -2,6 +2,7 @@ from errbot import BotPlugin
 import logging
 import re
 import requests
+from requests_kerberos import HTTPKerberosAuth, DISABLED
 
 
 class Jira(BotPlugin):
@@ -12,42 +13,60 @@ class Jira(BotPlugin):
     def get_configuration_template(self):
         """Defines the configuration structure this plugin supports"""
         return {'URL': "http://jira.example.com",
-                'USERNAME': 'err',
-                'PASSWORD': 'secret',
                 'PROJECTS': ['FOO', 'BAR']}
+
+    def get_cookie(self):
+        r = requests.get(self.config['URL']+'/step-auth-gss',
+                         auth=HTTPKerberosAuth(mutual_authentication=DISABLED),
+                         verify=False)
+        return r.cookies['JSESSIONID']
 
     def get_issue(self, issue_id):
         """Retrieves issue JSON from JIRA"""
+        try:
+            cookie = self.get_cookie()
+            logging.info("[JIRA] got cookie %s" % cookie)
+        except:
+            cookie = ''
         response = requests.get(
             self.config['URL']+'/rest/api/latest/issue/'+issue_id+'.json',
-            auth=(self.config['USERNAME'], self.config['PASSWORD']))
+            headers={"Cookie": "JSESSIONID=%s" % cookie}, verify=False)
+        logging.info("[JIRA] got response %s" % response.status_code)
         return response
 
-    def callback_message(self, conn, mess):
-        """A callback which responds to mention of JIRA issues"""
+    def get_response_text(self, mess):
         if self.config:
             matches = []
             regexes = []
             for project in self.config['PROJECTS']:
                 regexes.append(r'(%s\-[0-9]+)' % project)
             for regex in regexes:
-                matches.extend(re.findall(regex, mess.getBody(), flags=re.IGNORECASE))
+                matches.extend(re.findall(regex, mess.body, flags=re.IGNORECASE))
             if matches:
                 # set() gives us uniques, but does not preserve order.
                 for match in set(matches):
                     issue_id = match
-                    logging.debug("[JIRA] matched issue_id: %s" % issue_id)
+                    logging.info("[JIRA] matched issue_id: %s" % issue_id)
                     issue_response = self.get_issue(issue_id)
                     if issue_response.status_code in (200,):
-                        logging.debug("[JIRA] retrieved issue data: %s" % issue_response)
+                        logging.info("[JIRA] retrieved issue data: %s" % issue_response)
                         issue_summary = issue_response.json()['fields']['summary']
-                        html_message = "<html><body><a href=\"%s/browse/%s\">%s</a>: %s</body></html>" % (self.config['URL'], issue_id, issue_id, issue_summary,)
-                        self.send(mess.getFrom(), html_message, message_type=mess.getType())
+                        return "%s/browse/%s - %s" % (self.config['URL'], issue_id, issue_summary)
                     elif issue_response.status_code in (401,):
-                        self.send(mess.getFrom(), "Access Denied", message_type=mess.getType())
+                        logging.error("[JIRA] Access Denied")
                     elif issue_response.status_code in (404,):
-                        self.send(mess.getFrom(), "Issue not found", message_type=mess.getType())
+                        return "Issue not found"
                     else:
                         logging.error("[JIRA] encountered unknown response status code: %s" % issue_response.status_code)
                         logging.error("[JIRA] response body: %s" % issue_response.json())
-                        self.send(mess.getFrom(), "Recieved an unexpected response, see logs for more detail", message_type=mess.getType())
+
+    def callback_message(self, mess):
+        """A callback which responds to mention of JIRA issues"""
+        text_message = self.get_response_text(mess)
+        if not text_message:
+            return
+
+        if mess.is_direct:
+            self.send(mess.frm, text_message)
+        elif mess.is_group:
+            self.send(mess.frm.room, text_message)
